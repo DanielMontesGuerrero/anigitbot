5abb32b24be45c3c0b380614830315e91f2cc42b
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2022-01-15 15:31:12 -0600

    Usar Flask para el servidor

--- run_server.py
+++ run_server.py
@@ -1,12 +1,34 @@
 import sys
 import os
 
-from http.server import HTTPServer
-from utils.http_handler import Handler
-from functools import partial
+from flask import Flask, request
+from utils.anigitrest import Anigitrest
 
 
-def run():
+app = Flask('anigitbot_server')
+
+@app.route('/')
+async def serve():
+    try:
+        data = request.args
+        discord_token, github_token = config()
+        print(discord_token, github_token)
+        channel_id = int(data.get('channel_id'))
+        user = data.get('user')
+        repository_name = data.get('repo')
+        pr_number = int(data.get('pr'))
+        anigitrest = Anigitrest(discord_token=discord_token, github_token=github_token)
+        await anigitrest.notify_pull_request(
+            channel_id,
+            user,
+            repository_name,
+            pr_number,
+        )
+        return 'Success'
+    except KeyError:
+        return 'Missing argumetns'
+
+def config():
     if len(sys.argv) > 1:
         discord_token = sys.argv[1]
     else:
@@ -15,13 +37,8 @@ def run():
         github_token = sys.argv[2]
     else:
         github_token = os.getenv('GITHUB_TOKEN')
-    if len(sys.argv) > 3:
-        port = int(sys.argv[3])
-    else:
-        port = int(os.getenv('PORT'))
-    handler = partial(Handler, discord_token, github_token)
-    with HTTPServer(('', port), handler) as server:
-        server.serve_forever()
+    return discord_token, github_token
 
 if __name__ == '__main__':
-    run()
+    port = int(os.environ.get('PORT', '8000'))
+    app.run(host='0.0.0.0', port=port)
