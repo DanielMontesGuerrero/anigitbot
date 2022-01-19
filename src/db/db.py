e72d67be18dc9040be4a4c901391c4ba21727809
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2022-01-18 18:53:58 -0600

    Deshabilitar check same thread

--- src/db/db.py
+++ src/db/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///anigitbot.sqlite', check_same_thread=False)
+engine = create_engine('sqlite:///anigitbot.sqlite', connect_args={'check_same_thread': False})
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
