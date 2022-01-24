2a013c0d2d5c00fba7f33c7dfeee4c5a6ade3382
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2022-01-23 20:04:33 -0600

    Corregir prefijo en url

--- src/db/db.py
+++ src/db/db.py
@@ -6,6 +6,8 @@ from sqlalchemy.ext.declarative import declarative_base
 local_db = 'sqlite:///anigitbot.sqlite'
 
 url_db = os.environ.get('DATABASE_URL', local_db)
+if url_db and url_db.startswith("postgres://"):
+    url_db = url_db.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(url_db)
 Session = sessionmaker(bind=engine)
